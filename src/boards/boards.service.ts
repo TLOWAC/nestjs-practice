664f63fe3f57cafe2bbf19fd717e69a22588ce4a
chlee-tlowac <leechanghun55@gmail.com>
2022-01-25 22:12:37 +0900

    :bulb: chore: add comment

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -13,10 +13,19 @@ export class BoardsService {
   }
 
   getBoardById(id: string) {
-    const result = this.data.filter((board) => {
-      return board.id === id;
-    });
-    if (!result) {
+    const result = this.data.filter((board) => board.id === id);
+    /**
+     * @case1
+     * 일치하는 데이터가 없을 경우 데이터 반환 X
+     * const result = this.data.filter((board) => board.id === id);
+     *
+     * @case2
+     * 일치하는 데이터가 없을 경우 [] 빈배열을 반환
+     * const result = this.data.filter((board) => {
+     * return board.id === id;
+     * });
+     */
+    if (result) {
       throw new NotFoundException(`There is ${id}}`);
     }
     return result;
