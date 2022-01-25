5a241ee0f6a56f0aee2efef5c06e09aa709c987c
chlee-tlowac <leechanghun55@gmail.com>
2022-01-25 22:12:04 +0900

    refactor(board): module > add typeorm configuration

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports: [TypeOrmModule.forRoot()],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
