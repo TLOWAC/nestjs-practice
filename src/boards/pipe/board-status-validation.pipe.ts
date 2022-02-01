be63057558610d5a4c879d4ac8b22448d658b758
chlee-tlowac <leechanghun55@gmail.com>
2022-02-01 21:38:09 +0900

    :sparkles: feat(pipe): add status vadliation custom pipe

---
+++ src/boards/pipe/board-status-validation.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatusEnumType } from '../boards.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly boardStatusOptions = [
+    BoardStatusEnumType.PUBLIC,
+    BoardStatusEnumType.PRIVATE,
+  ];
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+
+    if (!this.isValidate(value))
+      throw new BadRequestException(`${value} is not existed in status type}`);
+
+    return value;
+  }
+
+  isValidate(value) {
+    const result = this.boardStatusOptions.includes(value);
+    return result;
+  }
+}
