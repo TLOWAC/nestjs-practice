d7ed1e1a73f31c25e8a517baab2da97f414ab9d2
chlee-tlowac <leechanghun55@gmail.com>
2022-02-01 21:33:16 +0900

    :bulb: chore: add comment about global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { BoardsModule } from './boards/boards.module';
@@ -14,11 +14,32 @@ async function bootstrap() {
     .setBasePath('api')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(3306, () => {
-    logger.log('server running on http://localhost:3306');
+  /**
+   *  whitelist
+   * : DTO 에 정의되지 안은 값은 필터링되며 사용자가 값을 입력하였더라도 API 에서는 이를 필터링한다.
+   *
+   * forbidNonWhitelisted
+   * : DTO 에 정의되어 있지 않은 값이 존재하는 경우 에러를 발생시킵니다.
+   *
+   * transform
+   * : 각종 request 요청을 (json) DTO 로 변환합니다.
+   *
+   * disableErrorMessages
+   * : 에러가 발생하였을때 에러 메시지의 표시 여부를 설정합니다.
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(3030, () => {
+    logger.log('server running on http://localhost:3030');
   });
 }
 bootstrap();
