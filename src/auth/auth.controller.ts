6279ec141666ec6ca85ed1b77a94312ed8b47731
chlee-tlowac <leechanghun55@gmail.com>
2022-02-02 01:44:05 +0900

    feat(controller): auth controller init

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() authCredntialDto: AuthCredentialDto) {
+    const user = this.authService.signUp(authCredntialDto);
+    return user;
+  }
+}
