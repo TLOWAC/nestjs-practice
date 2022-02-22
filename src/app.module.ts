f0af17ad60fcd715e0355a6d4e1b7d2d46843c00
chlee-tlowac <leechanghun55@gmail.com>
2022-02-22 20:48:52 +0900

    refactor(module): add schedulerModule to root appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { CoreModule } from './@common/libs/core.module';
 import { AuthModule } from './auth/auth.module';
 import { BoardsModule } from './boards/boards.module';
 import { MailModule } from './mail/mail.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { MailModule } from './mail/mail.module';
     BoardsModule,
     MailModule,
     CoreModule,
+    SchedulerModule,
   ],
 })
 export class AppModule {}
