f00842ad2071800410d37c502db8a3870f8922f5
chlee-tlowac <leechanghun55@gmail.com>
2022-02-24 21:30:57 +0900

    refactor(module): app > add ScheduleModule.forRoot()

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CoreModule } from './@common/libs/core.module';
 import { AuthModule } from './auth/auth.module';
 import { BoardsModule } from './boards/boards.module';
 import { MailModule } from './mail/mail.module';
-import { SchedulerModule } from './scheduler/scheduler.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    ScheduleModule.forRoot(),
     AuthModule,
     BoardsModule,
     MailModule,
     CoreModule,
-    SchedulerModule,
+    TaskModule,
   ],
 })
 export class AppModule {}
