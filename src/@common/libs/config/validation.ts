1ebc1e108ba8831cfd2041bb6d78c1aa3119d6c2
chlee-tlowac <leechanghun55@gmail.com>
2022-02-25 14:21:44 +0900

    refactor: .env validation

--- src/@common/libs/config/validation.ts
+++ src/@common/libs/config/validation.ts
@@ -1,6 +1,6 @@
 import * as Joi from 'joi';
 
 export const validationEnvSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('local', 'development', 'production', 'test'),
+  NODE_ENV: Joi.string().valid('local', 'dev', 'prod', 'test'),
   PORT: Joi.number().default(3000),
 });
